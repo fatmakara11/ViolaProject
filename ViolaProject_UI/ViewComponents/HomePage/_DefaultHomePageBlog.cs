087663fe996a8e827592213bbffba20f040a8622
fatmakara11 <fatmaka646@gmail.com>
2024-07-31 09:13:27 +0300

    anasayfadaki view componentlerin tamamlanması

---
+++ ViolaProject_UI/ViewComponents/HomePage/_DefaultHomePageBlog.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViolaProject_UI.ViewComponents.HomePage
+{
+    public class _DefaultHomePageBlog:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+        return View(); 
+        }   
+    }
+    
+}
